9a27661ce0007ad8cbeb03a66ad86be1b649b6fa
Martynkin Slavamir <sss_mmm86@mail.ru>
2024-03-24 20:01:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using System.Xml;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonStr = "{\"name\":\"Слава\",\"age\":38,\"city\":\"СПБ\"}";
+
+            var json = JsonDocument.Parse(jsonStr);
+
+            var xml = new XmlDocument();
+            var xmlRoot = xml.CreateElement("root");
+            xml.AppendChild(xmlRoot);
+
+            ConvertJson2Xml(json.RootElement, xmlRoot);
+            Console.WriteLine(xml.OuterXml);
+        }
+
+        static void ConvertJson2Xml(JsonElement jsonElement, XmlElement xmlParent)
+        {
+            foreach (var prop in jsonElement.EnumerateObject())
+            {
+                if (prop.Value.ValueKind == JsonValueKind.Object)
+                {
+                    var xmlElement = xmlParent.OwnerDocument.CreateElement(prop.Name);
+                    xmlParent.AppendChild(xmlElement);
+                    ConvertJson2Xml(prop.Value, xmlElement);
+                }
+                else
+                {
+                    var xmlElement = xmlParent.OwnerDocument.CreateElement(prop.Name);
+                    xmlElement.InnerText = prop.Value.ToString();
+                    xmlParent.AppendChild(xmlElement);
+                }
+            }
+        }
+    }
+}
